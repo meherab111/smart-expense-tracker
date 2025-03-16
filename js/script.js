0ee756648c57db70fe6c94955f1fa854232b5893
meherab111 <mehrabborno@gmail.com>
2025-03-17 02:08:39 +0600

    updated

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let expenseNameField = document.querySelector(".expense-name--field")
+
+let amountField = document.querySelector(".amount-field")
+
+let selectField = document.querySelector(".select-field")
+
+let datetimeField = document.querySelector(".datetime-field")
+
+let addButton = document.getElementById("add-button")
+
+
+
+class ExpenseTracker{
+
+    addExpense(expenseName, amount, select, dateTime){
+
+        this.expenseName = expenseName
+        this.amount = amount
+        this.select = select
+        this.dateTime = dateTime
+        
+    }
+
+}
+
+
+
+
+let expenses = new ExpenseTracker()
+
+
+
+addButton.addEventListener("click", () => {
+    
+    let formattedDate = new Date(datetimeField.value)
+
+    let newMonth = formattedDate.getMonth()+1
+
+    let newDay = formattedDate.getDate()
+
+    let newTime = formattedDate.toLocaleTimeString()
+
+    let newYear = formattedDate.getFullYear()
+
+    let dateTimeFormatted = `${newMonth}-${newDay}-${newYear} | ${newTime}`
+
+    expenses.addExpense(expenseNameField.value, amountField.value,selectField.value, dateTimeFormatted)
+   
+})
