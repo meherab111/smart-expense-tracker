17d8050c4ba7d783e8e4a64b67b3d34162d836aa
meherab111 <mehrabborno@gmail.com>
2025-03-18 13:33:30 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,61 +1,101 @@
-let expenseNameField = document.querySelector(".expense-name--field")
+// Input Fields
+let expenseNameField = document.querySelector(".expense-name--field");
 
-let amountField = document.querySelector(".amount-field")
+let amountField = document.querySelector(".amount-field");
 
-let selectField = document.querySelector(".select-field")
+let selectField = document.querySelector(".select-field");
 
-let datetimeField = document.querySelector(".datetime-field")
+let datetimeField = document.querySelector(".datetime-field");
 
-let addButton = document.getElementById("add-button")
+let addButton = document.getElementById("add-button");
 
-let newArr = []
+let newArr = [];
 
-// Class 
+// Template
 
-class ExpenseTracker{
+let dynamicExpenseList = document.querySelector(".expense-list--div");
 
-    addExpense(expenseName, amount, select, dateTime){
+let expenseTemplate = document.getElementById("expense-template");
 
-        this.expenseName = expenseName
-        this.amount = amount
-        this.select = select
-        this.dateTime = dateTime
+let expenseTemplateClone = expenseTemplate.content.cloneNode(true);
 
-        newArr.push(this.expenseName, this.amount, this.select, this.dateTime)
-        
-    }
+// Template Tags
 
-}
+let dateTimeTag = document.querySelector(".datetime-tag");
 
-class ExpenseOperation extends ExpenseTracker{
+let expenseInfoTag = document.querySelector(".expense-info--tag");
+
+let amountTag = document.querySelector(".amount-tag");
+
+let removeButton = document.getElementById("remove-button");
+
+// Class
+
+class ExpenseOperation {
+  constructor(expenseName, amount, select, dateTime) {
+    this.expenseName = expenseName;
+    this.amount = amount;
+    this.select = select;
+    this.dateTime = dateTime;
     
-        loadExpense(){
-            console.log(newArr);
-        }
+  }
 
+  setExpense() {
+    newArr.push(this.expenseName, this.amount, this.select, this.dateTime);
+
+    let convertedArr = JSON.stringify(newArr);
+
+    localStorage.setItem("expense-data", convertedArr) || [];
+  }
+
+  getExpense() {
+    let storageData = localStorage.getItem("expense-data");
+
+    let convertedData = JSON.parse(storageData);
+
+    return convertedData;
+  }
+
+  removeExpense() {
+    localStorage.removeItem("expense-data");
+  }
 }
 
+class ExpenseDisplay extends ExpenseOperation {
+  displayExpense() {
+    let x = super.getExpense();
 
-
-
-let expenses = new ExpenseTracker()
+    x.forEach((element) => {
+      console.log(element);
+    });
+  }
+}
 
 // Click Event
 
 addButton.addEventListener("click", () => {
-    
-    let formattedDate = new Date(datetimeField.value)
+  let formattedDate = new Date(datetimeField.value);
 
-    let newMonth = formattedDate.getMonth()+1
+  let newMonth = formattedDate.getMonth() + 1;
 
-    let newDay = formattedDate.getDate()
+  let newDay = formattedDate.getDate();
 
-    let newTime = formattedDate.toLocaleTimeString()
+  let newTime = formattedDate.toLocaleTimeString();
 
-    let newYear = formattedDate.getFullYear()
+  let newYear = formattedDate.getFullYear();
 
-    let dateTimeFormatted = `${newMonth}-${newDay}-${newYear} | ${newTime}`
+  let dateTimeFormatted = `${newMonth}-${newDay}-${newYear} | ${newTime}`;
 
-    expenses.addExpense(expenseNameField.value, amountField.value,selectField.value, dateTimeFormatted)
-   
-})
+  let expenseOp = new ExpenseOperation(
+    expenseNameField.value,
+    amountField.value,
+    selectField.value,
+    dateTimeFormatted
+  );
+
+  expenseOp.setExpense();
+
+  let expenseDis = new ExpenseDisplay();
+
+  expenseDis.displayExpense();
+});
